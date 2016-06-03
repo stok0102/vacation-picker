8bed201dc76754e9ebc541262fa36a9218e131da
Caleb Stokka <caleb.stokka@gmail.com>
2016-06-03 10:14:59 -0700

    add branching to determine destination

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,35 @@
 
 //Front End Logic
 $(document).ready(function() {
-  $("form").submit(event){
+  $("form").submit(function(event){
     event.preventDefault();
 
     var passport = $("#passport").val();
+    var recInput = parseInt($("#rec").val());
+    var reasonInput = parseInt($("#reason").val());
+    var budgetInput = parseInt($("#budget").val());
+    var foodInput = parseInt($("#food").val());
+    var score = recInput + budgetInput + reasonInput +foodInput
     debugger;
+    if (passport === "Yes") {
+      if (score >= 1) {
+        $("#patagonia").siblings().hide();
+        $("#patagonia").show();
+      }
+
+      else if (score < 1) {
+        $("#morocco").siblings().hide();
+        $("#morocco").show();
+      }
+
+      else {
+        alert("You missed a box");
+      }
+    }
+
+    else {
+      $("#hayward").siblings().hide();
+      $("#hayward").show();
+    }
   });
 });
